70df0f8785670b46dffdf17577109b9c69b07171
Sparsh Bansal <sparsh.bansal17895@gmail.com>
2018-04-29 08:17:07 +0530

    Saving Model

--- train.py
+++ train.py
@@ -5,6 +5,12 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
+model_dir = './model/'
+model_name = 'gan_mnist'
+
+save_step = 1000
+out_step = 10000
+
 def generator(z):
 
     input_dims = z.shape[1]
@@ -91,19 +97,47 @@ d_opitmizer = tf.train.AdamOptimizer()
 d_train_step = d_opitmizer.minimize(d_loss, var_list=disc_vars)
 
 batch_size=128
+z_dim = 100
+
+saver = tf.train.Saver()
 
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    
+    # write the graph for tensorboard
     writer= tf.summary.FileWriter('./graph/')
     writer.add_graph(sess.graph)
-    
-    # now train generator and discriminator alternatively 
-    for it in range(1000000):
-        X_batch , _ = mnist.train.next_batch(batch_size)
-        _ , _ , g_loss_current, d_loss_current = sess.run([d_train_step, g_train_step, g_loss, d_loss] , feed_dict={X: X_batch, Z:sample_z([batch_size, 100])})
 
-        if it % 1000 == 0:
-            print('Iter: {}'.format(it))
-            print('D loss: {:.4}'. format(d_loss_current))
-            print('G_loss: {:.4}'.format(g_loss_current))
-            print()
+    # try loading a saved model , if not found initialize a new one 
+    try:
+        saver.restore(sess, model_dir + model_name)
+        print "Restoring saved model"
+
+    except (ValueError, tf.errors.NotFoundError):
+        print "Creating new model"
+        sess.run(tf.global_variables_initializer())
+
+    # now train generator and discriminator alternatively 
+    for it in range(10000000):
+        X_batch , _ = mnist.train.next_batch(batch_size)
+        _ , _ , g_loss_current, d_loss_current = sess.run([d_train_step, g_train_step, g_loss, d_loss] , feed_dict={X: X_batch, Z:sample_z([batch_size, z_dim])})
+    
+        if it%1000 == 0:
+            print "Iteration : {} , Generator Loss : {} , Discriminator Loss : {}".format(it, g_loss_current, d_loss_current)
+
+
+        # every save_step iterations save the model
+        if it%save_step == 0:
+            saver.save(sess, model_dir + model_name)
+        
+        # every out_step save the output 
+        if it%out_step == 0:
+            generated_images = sess.run(gene_out , feed_dict={Z : sample_z([batch_size, z_dim])})
+            generated_images.reshape([batch_size, 28, 28])
+            
+            for img_idx in range(generated_images.shape[0]):
+                img = generated_images[img_idx]
+                im = Image.frombytes('L' , (28,28), img)
+                imname = './output/im_{}_{}.png'.format(it , img_idx)
+                im.save(imname)
+
+
